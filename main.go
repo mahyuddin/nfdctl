6a219bb253e03d49764202e9a2ea6367d66f51e4
mahyuddin <mahyuddin@gmail.com>
2015-09-13 22:03:10 +0800

    Initial files to start development
    main.go
    key/

---
+++ main.go
@@ -0,0 +1,35 @@
+package nfdctl
+
+import (
+	"os"
+	"time"
+
+	"github.com/go-ndn/log"
+	"github.com/go-ndn/mux"
+	"github.com/go-ndn/ndn"
+	"github.com/go-ndn/packet"
+	"github.com/go-ndn/persist"
+)
+
+func func main() {
+	
+	// connect to nfd
+	conn, err := packet.Dial("tcp", ":6363")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// create a new face
+	recv := make(chan *ndn.Interest)
+	face := ndn.NewFace(conn, recv)
+	defer face.Close()
+
+	// read producer key
+	pem, err := os.Open("key/default.pri")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer pem.Close()
+	key, _ := ndn.DecodePrivateKey(pem)
+	
+}
